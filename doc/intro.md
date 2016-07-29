431b7fe1847d9550d3c2110e4ee885a962e7b1e5
niquola <niquola@gmail.com>
2016-07-30 00:43:48 +0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to predsedatel
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/predsedatel/core_test.clj
@@ -0,0 +1,7 @@
+(ns predsedatel.core-test
+  (:require [clojure.test :refer :all]
+            [predsedatel.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
